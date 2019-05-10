8ff76df51b3cf5047b8508c2974c8810df5693f4
Francislin Dos Reis Souza <francislin.souza@lmcorp.local>
2019-05-10 17:34:13 -0300

    Criando a classe do usuario

---
+++ src/main/java/br/com/itau/desafioitau/core/repository/user/model/UserEntity.java
@@ -0,0 +1,16 @@
+package br.com.itau.desafioitau.core.repository.user.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Francislin Dos Reis on 12/12/17.
+ */
+@Data
+@Entity
+@Table(name = "tab_user")
+public class UserEntity {
+
+}
