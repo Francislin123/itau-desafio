cf0c5c84bee3e57ff54511e3e2ce073417dc213a
Francislin Dos Reis Souza <francislin.souza@lmcorp.local>
2019-05-10 16:51:20 -0300

    First commit

---
+++ src/test/java/br/com/itau/desafioitau/DesafioItauApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.itau.desafioitau;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesafioItauApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
